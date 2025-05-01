7c47260b10bcc99ee99f5f649347c76d2cb60af7
sanyamkashyap <sanyamkashyap40@gmail.com>
2025-05-01 16:18:11 +0530

    writing test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
-import {Test,console} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/fundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
@@ -9,23 +9,22 @@ contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-      // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-      DeployFundMe deployFundMe = new DeployFundMe();
-      fundMe = deployFundMe.run();
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollerIsFive() public view {
-       assertEq(fundMe.MINIMUM_USD(),5e18);
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
-    
-    function testOwnerIsMsgSender() public view{
+
+    function testOwnerIsMsgSender() public view {
         console.log(fundMe.i_owner());
         console.log(msg.sender);
-        assertEq(fundMe.i_owner(),msg.sender);
-
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
-        function testPriceFeedVersionIsAccurate() public view {
+    function testPriceFeedVersionIsAccurate() public view {
         if (block.chainid == 11155111) {
             uint256 version = fundMe.getVersion();
             assertEq(version, 4);
@@ -33,5 +32,14 @@ contract FundMeTest is Test {
             uint256 version = fundMe.getVersion();
             assertEq(version, 6);
         }
-  }        
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        fundMe.fund{value: 10e18}();
+    }
 }
